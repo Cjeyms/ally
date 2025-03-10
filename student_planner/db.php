f4cc5b97140a5d2df60281516dc9ff57f73b504d
James <143634044+Cjeyms@users.noreply.github.com>
2025-03-11 01:47:31 +0800

    Update db.php

--- student_planner/db.php
+++ student_planner/db.php
@@ -1,13 +1,13 @@
 <?php
-$host = "$sql109.infinityfree.com";
-$user = "$if0_38489351"; // Default XAMPP MySQL user
-$pass = "$U5C0nDruJu"; // No password by default
-$dbname = "$if0_38489351_student_planner";
+$host = "localhost";
+$user = "root"; // Default XAMPP MySQL user
+$pass = ""; // No password by default
+$dbname = "student_planner";
 
-// Create connection
-$conn = new mysqli($host, $user, $pass, $dbname);
+// // Create connection
+// $conn = new mysqli($host, $user, $pass, $dbname);
 
-// $conn = new mysqli($sql109.infinityfree.com, $if0_38489351, $U5C0nDruJu, database: $if0_38489351_student_planner);
+$conn = new mysqli($sql109.infinityfree.com, $if0_38489351, $U5C0nDruJu, database: $if0_38489351_student_planner);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
